9fdd0cf994691213233a5e83831ad083ea8202ad
Leonel Peña <leo.dhpl@gmail.com>
2019-04-16 17:31:51 -0400

    Docs: fixed url and project name in site config.

--- docs/website/siteConfig.js
+++ docs/website/siteConfig.js
@@ -23,14 +23,14 @@ const users = [
 const siteConfig = {
   title: 'uStart', // Title for your website.
   tagline: 'NodeJS framework for building GraphQL backends using Apollo, Sequelize, Mongoose and other great tools',
-  url: 'https://ustart.dev', // Your website URL
+  url: 'https://ustart-dev.github.io', // Your website URL
   baseUrl: '/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  projectName: 'ustart.github.io',
+  projectName: 'ustart-dev.github.io',
   organizationName: 'ustart-dev',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
